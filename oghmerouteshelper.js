8f066e3f02f4ed81fad4ef19f008c1be9361bc5c
Peter Host <peterhost+github@virtualregions.com>
2011-12-13 13:10:11 +0100

    moving on

--- oghmerouteshelper.js
+++ oghmerouteshelper.js
@@ -1,14 +1,18 @@
-//
-//  customroutes.js
-//  NODE
-//
-//  Created by Peter Host on 2010-10-25.
-//  Copyright 2010 OGHME.COM. All rights reserved.
-//
+/*
+ *
+ *  NAME: customroutes.js
+ *
+ *  Created by Peter Host on 2010-10-25.
+ *  Copyright 2010 OGHME.COM. All rights reserved.
+ *
+ *
+ */
+
 
 /*
     TODO
 */
+
 
 //var md = require('markdown');
 var fs = require('fs');
@@ -39,85 +43,81 @@ function typeOf(value) {
 
 
 
+/*
+ *-----------------------------------------------------------------------
+ *                             GENERIC
+ *-----------------------------------------------------------------------
+ */
 
-
-
-
-
-
-
-
-
-//<!--######################################################################-->
-//---
-// ## GENERIC
-//<!--//////////////////////////////////////////////////////////////////////-->
-// ### ROUTE LISTING
-//<!--______________________________________________________________________-->
-// #### routeList
-// returns a list containing all the routes to resources of one type (for the mo. file extension is the type)
-// within a given path (directory, subdirectories,...)
-// [
-//   {file: 'somearticle', route: [ 'tech', 'js']},
-//   {file: 'otherarticle', route: [ 'books']},
-//   {file: 'yetanotherarticle'},                           first level element, no route array
-// ]
+//_______________________________________________________________________
+//                            ROUTE LISTING
+//
+//  returns a list containing all the routes to resources of one type (for the mo. file extension is the type)
+//  within a given path (directory, subdirectories,...)
+//  [
+//    {file: 'somearticle', route: [ 'tech', 'js']},
+//    {file: 'otherarticle', route: [ 'books']},
+//    {file: 'yetanotherarticle'},                           first level element, no route array
+//  ]
+//
 function routeList(path, extension) {
-    var artlist,
-        i,
-        pathreg = new RegExp(path.replace(/([^\/])$/, '$1/') + '|\\.' + extension, 'g'),
-        // pathreg removes : 1) initial DIR path from a string, 2) AND the `.extension` from the end of a string
-        // the .replace part removes the trailing slash from `path` as slash will be the splitting delimiter
-        routes = []; // array of the different routes available in the category ARTICLE
+  var artlist,
+      i,
+      pathreg = new RegExp(path.replace(/([^\/])$/, '$1/') + '|\\.' + extension, 'g'),
+      // pathreg removes : 1) initial DIR path from a string, 2) AND the `.extension` from the end of a string
+      // the .replace part removes the trailing slash from `path` as slash will be the splitting delimiter
+      routes = []; // array of the different routes available in the category ARTICLE
 
-    function mkroute(thisPath) {
-        var localURI = thisPath.replace(pathreg, ""),
-        tmparr = [],
-        tmpobj = {};
-        tmparr = localURI.split('/'); // split the path
-        tmpobj.file = tmparr.pop(); // the last element is the filename radical
-        if (tmparr.length > 0) { // the remaining of the array is the route
-            tmpobj.route = tmparr;
-        }
-        routes.push(tmpobj);
+  function mkroute(thisPath) {
+    var localURI = thisPath.replace(pathreg, ""),
+    tmparr = [],
+    tmpobj = {};
+    tmparr = localURI.split('/'); // split the path
+    tmpobj.file = tmparr.pop(); // the last element is the filename radical
+    if (tmparr.length > 0) { // the remaining of the array is the route
+        tmpobj.route = tmparr;
     }
+    routes.push(tmpobj);
+  }
 
-    // valid dirpath ?
-    fs.stat(path, function (err, stat) {
-        if (err) {
-            throw err;
-        }
-        if (!stat.isDirectory())  {
-            throw new Error("ERROR : [" + path + "] is not a valid dirpath");
-        }
-    });
-
-    // valid file extension string ?
-    if (!extension || typeOf(extension) !== "string" || !/^\w+$/.test(extension)) {
-        throw "ERROR : invalid extension " + extension;
+  // valid dirpath ?
+  fs.stat(path, function (err, stat) {
+    if (err) {
+        throw err;
     }
-
-    // fetch the filelist
-    artlist = fp.rparsefSYNC(
-        path,
-        fp.matcher(extension)
-    );
-
-    // extract the info
-    for (i = 0; i < artlist.length; i += 1) {
-        mkroute(artlist[i]);
+    if (!stat.isDirectory())  {
+        throw new Error("ERROR : [" + path + "] is not a valid dirpath");
     }
+  });
 
-    // stick 2 properties to the object
-    routes.path = path;
-    routes.extension = extension;
-    return routes;
+  // valid file extension string ?
+  if (!extension || typeOf(extension) !== "string" || !/^\w+$/.test(extension)) {
+    throw "ERROR : invalid extension " + extension;
+  }
+
+  // fetch the filelist
+  artlist = fp.rparsefSYNC(
+      path,
+      fp.matcher(extension)
+  );
+
+  // extract the info
+  for (i = 0; i < artlist.length; i += 1) {
+    mkroute(artlist[i]);
+  }
+
+  // stick 2 properties to the object
+  routes.path = path;
+  routes.extension = extension;
+  return routes;
 }
 
 
-//<!--______________________________________________________________________-->
-//#### sortTidiedRouteList
-// sort an array generated by tidyroutes()
+//_______________________________________________________________________
+//                           ROUTE SORTING
+//
+//  sortTidiedRouteList
+//  sort an array generated by tidyroutes()
 function sortTidiedRouteList(a, b) {
     if (a.route < b.route) {
         return -1;
@@ -132,8 +132,10 @@ function sortTidiedRouteList(a, b) {
 //objs.sort(sortTidiedRouteList);
 
 
-//<!--______________________________________________________________________-->
-// #### tidyroutes
+//_______________________________________________________________________
+//                         ROUTE TIDYING
+//
+// tidyroutes
 // orders a file-centric routes list obtained with routeList()
 // [
 //      { file: 'somearticle'   ,  route: [ 'tech', 'js']  },
@@ -147,66 +149,67 @@ function sortTidiedRouteList(a, b) {
 //      { route: 'books'    , files: ['otherarticle']                },
 //      { route: '.'     , files: ['yetanotherarticle']           }         '.': top level route
 // ]
+//
 function tidyroutes(routes) {
-    var newroutes = [], i;
+  var newroutes = [], i;
 
-    // populate one entry of the new `newroutes` array
-    function addFileToRoute(tmproute, tmpfile) {
-        var j,
-        initlength = newroutes.length; // store initial value for loop limit, as we will augment the array in the loop
-        //for (i = 0; i < .newroutes.length; i += 1) {
-        for (j = 0; j < 100; j += 1) {
-            if (newroutes[j] && newroutes[j].route && newroutes[j].route === tmproute) {
-                // route already there, append to files array
-                newroutes[j].files.push(tmpfile);
-                return;
-            }
-        }
-        // route not found, create it and assign it a singleton array containing tmpfile
-        newroutes.push({'route': tmproute, 'files': [tmpfile]});
+  // populate one entry of the new `newroutes` array
+  function addFileToRoute(tmproute, tmpfile) {
+    var j,
+    initlength = newroutes.length; // store initial value for loop limit, as we will augment the array in the loop
+    //for (i = 0; i < .newroutes.length; i += 1) {
+    for (j = 0; j < 100; j += 1) {
+      if (newroutes[j] && newroutes[j].route && newroutes[j].route === tmproute) {
+        // route already there, append to files array
+        newroutes[j].files.push(tmpfile);
+        return;
+      }
     }
+    // route not found, create it and assign it a singleton array containing tmpfile
+    newroutes.push({'route': tmproute, 'files': [tmpfile]});
+  }
 
 
-    function routeConcat(oldroute) { // oldroute ~ {file: 'somearticle', route: [ 'some', 'route']},
-        var concat = '', rtlist, j;
-        // top level route
-        if (!oldroute.route) { // (no 'route' property)
-            addFileToRoute('.',  oldroute.file);
-        }
-        // other routes
-        else {
-            rtlist = oldroute.route;
-            // concatenate array rtlist into a route
-            for (j = 0; j < rtlist.length; j += 1) {
-                concat += rtlist[j];
-                if (j < rtlist.length - 1) { // only add a slash __between__ route segments
-                    concat += '/';
-                }
-            }
-            addFileToRoute(concat, oldroute.file);
-        }
+  function routeConcat(oldroute) { // oldroute ~ {file: 'somearticle', route: [ 'some', 'route']},
+    var concat = '', rtlist, j;
+    // top level route
+    if (!oldroute.route) { // (no 'route' property)
+        addFileToRoute('.',  oldroute.file);
     }
-
-
-    // transform the old route list into the new one
-    for (i = 0; i < routes.length; i += 1) {
-        routeConcat(routes[i]);
+    // other routes
+    else {
+      rtlist = oldroute.route;
+      // concatenate array rtlist into a route
+      for (j = 0; j < rtlist.length; j += 1) {
+        concat += rtlist[j];
+        if (j < rtlist.length - 1) { // only add a slash __between__ route segments
+          concat += '/';
+        }
+      }
+      addFileToRoute(concat, oldroute.file);
     }
-    newroutes.sort(sortTidiedRouteList);
+  }
 
-    // copy the original 'routes' array's properties
-    newroutes.path = routes.path;
-    newroutes.extension = routes.extension;
 
-    llog.l(newroutes);
-    return newroutes;
+  // transform the old route list into the new one
+  for (i = 0; i < routes.length; i += 1) {
+    routeConcat(routes[i]);
+  }
+  newroutes.sort(sortTidiedRouteList);
+
+  // copy the original 'routes' array's properties
+  newroutes.path = routes.path;
+  newroutes.extension = routes.extension;
+
+  llog.l(newroutes);
+  return newroutes;
 }
 
 
-//<!--//////////////////////////////////////////////////////////////////////-->
-// ### METADATA
-//<!--______________________________________________________________________-->
-// #### getMeta (SYNC)
+//_______________________________________________________________________
+//                         METADATA
+//
+// getMeta (SYNC)
 function getMeta(basepath, route, file, ext) {
     var tmprt = route === '.' ? '' : route + '/',
         content = fs.readFileSync( basepath + '/' + tmprt + file + '.' + ext, 'utf8');
@@ -221,14 +224,17 @@ function getMeta(basepath, route, file, ext) {
 
 
 
-//<!--######################################################################-->
-//---
-// ## HTML functions
-//<!--//////////////////////////////////////////////////////////////////////-->
-// ### GENERIC
+/*
+ *-----------------------------------------------------------------------
+ *                          HTML FUNCTIONS
+ *-----------------------------------------------------------------------
+ */
 
-//<!--______________________________________________________________________-->
-// #### toHTMLtree
+//_______________________________________________________________________
+//                         GENERIC
+//
+// toHTMLtree
+//
 // returns a HTML tree representing a list of routes, from a 'tidied' route array
 // (cf. tidyroutes())
 // [
@@ -255,11 +261,11 @@ function toHTMLtree(tidyRoutes) {
     return HTML;
 }
 
-//<!--//////////////////////////////////////////////////////////////////////-->
-// ### ARTICLES
-
-//<!--______________________________________________________________________-->
-// #### md_routeList
+//_______________________________________________________________________
+//                         ARTICLES
+//
+// md_routeList
+//
 // returns a HTML index of 'markdown' files in a given directory
 function md_routeList(path) {
     var list = routeList(path, 'markdown');
@@ -274,3 +280,4 @@ function md_routeList(path) {
 
 
 exports.md_routeList = md_routeList;
+exports.routeList = routeList;
